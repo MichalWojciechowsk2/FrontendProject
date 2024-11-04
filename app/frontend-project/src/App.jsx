04fb45e180466660702f2a1099fd348c3315c87b
Michał Wojciechowski <michalwojogolne@gmail.com>
2024-11-05 00:42:26 +0100

    lab04.AddForm

--- app/frontend-project/src/App.jsx
+++ app/frontend-project/src/App.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import NotFound from "./pages/NotFound";
 import menuItems from "./data/menu-items";
 import RootLayout from "./layouts/RootLayout";
 import { Routes, Route } from "react-router-dom";
+import AppContext from "./data/AppContext";
+import AppReducer from "./data/AppReducer";
+import { data } from "./data/module-data.jsx";
+import FlexContainer from "./components/FlexContainer";
 
 function App() {
+  const initialState = { items: data };
+  const [state, appDispatch] = useReducer(AppReducer, initialState);
   return (
     <>
-      <RootLayout items={menuItems}>
-        <Routes>
-          {menuItems.map((item) => (
-            <Route
-              key={item.id}
-              path={item.urlPattern}
-              element={item.element}
-            />
-          ))}
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </RootLayout>
+      <AppContext.Provider
+        value={{ items: state.items, dispatch: appDispatch }}
+      >
+        <RootLayout items={menuItems}>
+          <Routes>
+            {menuItems.map((item) => (
+              <Route
+                key={item.id}
+                path={item.urlPattern}
+                element={item.element}
+              />
+            ))}
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </RootLayout>
+      </AppContext.Provider>
     </>
   );
 }
--- app/frontend-project/src/components/FlexContainer.jsx
+++ app/frontend-project/src/components/FlexContainer.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Card } from "react-bootstrap"; // Dodano import komponentu Card
+import React, { useContext } from "react";
+import { Card } from "react-bootstrap";
+import AppContext from "../data/AppContext";
 
-const FlexContainer = ({ element: Element, data }) => {
+const FlexContainer = ({ element: Element }) => {
+  // const { items, dispatch } = useContext(AppContext);
+  const context = useContext(AppContext);
+  const items = context.items;
   return (
     <div className="d-flex flex-wrap">
-      <div></div>
-      {data.map((car) => (
+      {items.map((car) => (
         <Card
           style={{ width: `18rem` }}
           className="border mb-3 p-3 ms-3"
